427884cc2d96bc892b6cf5d08b41eddf6a32dadb
David Baeza <david.isaac.baeza@gmail.com>
2021-03-09 22:42:28 -0600

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import About from "./AboutComponent";
-import Contact from "./ContactComponent";
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
-import { Icon } from "react-native-elements";
+import { Icon } from 'react-native-elements';
 import SafeAreaView from "react-native-safe-area-view";
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -193,6 +202,14 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
+
     render() {
         return (
             <View 
@@ -235,4 +252,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
